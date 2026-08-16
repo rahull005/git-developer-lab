3cb075e456f503f6b9e5785eb295590b3d6b09a5
rahull005 <rahulsuryasen2002@.com>
2026-08-16 22:18:57 +0530

    update welcome message

--- src/PracticeSet.java
+++ src/PracticeSet.java
@@ -2,7 +2,7 @@
 public class PracticeSet {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+        System.out.println("Hello and welcome to git!");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);

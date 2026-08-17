681eeb65109f62bb3178979db4e2a1e6b084ef12
rahulSuryasen <rahulsuryasen2002@gmail.com>
2026-08-17 14:29:46 +0530

    Add Git branch merging practice message

--- src/PracticeSet.java
+++ src/PracticeSet.java
@@ -5,6 +5,7 @@ public class PracticeSet {
         System.out.println("Hello and welcome to git and github!");
         System.out.println("Learning Git branches!");
         System.out.println("Practicing feature branches in Git!");
+        System.out.println("Practicing Git branch merging!");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);

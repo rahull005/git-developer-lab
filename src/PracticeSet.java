3057e285f62df6a43d2599e7c0a42fcc6f9e7d92
rahulSuryasen <rahulsuryasen2002@gmail.com>
2026-08-17 11:26:43 +0530

    Add git branch practice message

--- src/PracticeSet.java
+++ src/PracticeSet.java
@@ -3,6 +3,7 @@ public class PracticeSet {
     public static void main(String[] args) {
 
         System.out.println("Hello and welcome to git and github!");
+        System.out.println("Learning Git branches!");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
